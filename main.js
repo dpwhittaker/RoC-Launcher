e096945c820951a630d77f91cd34d0b8bfa6d33d
David Whittaker <dpwhittaker@gmail.com>
2017-07-10 15:27:41 -0500

    Add auto update support and build with electron-builder

--- main.js
+++ main.js
@@ -1,34 +1,42 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const {app, BrowserWindow, ipcMain, dialog} = require('electron');
+const log = require('electron-log');
+const {autoUpdater} = require('electron-updater');
+const path = require('path');
+const url = require('url');
 
-const path = require('path')
-const url = require('url')
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
-let mainWindow
+let mainWindow;
 
 function createWindow () {
-  mainWindow = new BrowserWindow({width: 1024, height: 600, show: false, autoHideMenuBar: true, frame: false})
+  mainWindow = new BrowserWindow({width: 1024, height: 600, show: false, autoHideMenuBar: true, frame: false});
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
-  //mainWindow.webContents.openDevTools()
-  mainWindow.once('ready-to-show', () => mainWindow.show())
-  mainWindow.once('closed', () => mainWindow = null)
+  }));
+  mainWindow.webContents.openDevTools()
+  mainWindow.once('ready-to-show', () => mainWindow.show());
+  mainWindow.once('closed', () => mainWindow = null);
 }
 
-app.on('ready', createWindow)
-app.on('window-all-closed', () => app.quit())
+app.on('ready', createWindow);
+app.on('window-all-closed', () => app.quit());
 
-const ipc = require('electron').ipcMain
-const dialog = require('electron').dialog
-
-ipc.on('open-directory-dialog', function (event) {
+ipcMain.on('open-directory-dialog', function (event) {
   dialog.showOpenDialog({
     properties: ['openDirectory']
   }, function (files) {
     if (files) event.sender.send('selected-directory', files[0])
-  })
-})
+  });
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  autoUpdater.quitAndInstall();  
+});
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdates();
+});
